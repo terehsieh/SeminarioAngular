0f5a7ddab89f1d4121912b857feb8016bcafcf6d
terehsieh <42392544+terehsieh@users.noreply.github.com>
2020-11-28 19:52:52 -0300

    eventos, agregado de aumentar y disminuir cantidad de productos

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,39 +7,52 @@ import { Product } from './Product';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-// declarar Json para usar en la lista 
+  // declarar Json para usar en la lista 
 
-products: Product[]=
-[{
-  name: 'Apple',
-  type: 'fruit',
-  price: 10,
-  stock: 0,
-  image: 'assets/img/apple.jpg',
-  clearance: true, // oferta
-  quantity:0, // por defecto 
-},
-{
-  name: 'Kiwi',
-  type: 'fruit',
-  price: 10,
-  stock: 20,
-  image: 'assets/img/apple.jpg',
-  clearance: false,
-  quantity:0,
-}]
+  products: Product[] =
+    [{
+      name: 'Apple',
+      type: 'fruit',
+      price: 10,
+      stock: 0,
+      image: 'assets/img/apple.jpg',
+      clearance: true, // oferta
+      quantity: 0, // por defecto 
+    },
+    {
+      name: 'Kiwi',
+      type: 'fruit',
+      price: 10,
+      stock: 20,
+      image: 'assets/img/apple.jpg',
+      clearance: false,
+      quantity: 0,
+    }]
   constructor() { }
 
   ngOnInit(): void {
   }
 
-upQuantity(product: Product): void{
-  if(product.stock> product.quantity)
-    product.quantity++;
-}
+  upQuantity(product: Product): void {
+    if (product.quantity < product.stock)
+      product.quantity++;
+  }
 
-downQuantity(product: Product): void{
-  if(product.quantity>0)
-    product.quantity--;
+  downQuantity(product: Product): void {
+    if (product.quantity > 0)
+      product.quantity--;
+  }
+
+  changeQuantity(e, product: Product): void {
+    // controlar q se ingresa numeros de 0 al 9
+    // if(event.key>0 && event.key<1000 ){
+    // product.quantity=event.key;
+    // }else{
+
+    //   product.quantity=0;
+    // }
+    
+    //https://stackoverflow.com/questions/41465542/angular2-input-field-to-accept-only-numbers/46754601
+console.log(e);
   }
 }
