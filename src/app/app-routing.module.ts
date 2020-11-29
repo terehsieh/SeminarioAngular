bb0a7f4b399e6e1c1d38fcd0dffa697c3e7f7340
terehsieh <42392544+terehsieh@users.noreply.github.com>
2020-11-29 00:25:32 -0300

    add routing (about, products)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+import { HimalayaAboutComponent } from './himalaya-about/himalaya-about.component';
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'products',
+    pathMatch: 'full'
+  },
+  {
+  path:'products',
+  component: ProductListComponent
+},
+{
+  path:'abouts',
+  component: HimalayaAboutComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
