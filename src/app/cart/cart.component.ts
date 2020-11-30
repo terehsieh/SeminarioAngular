6cd6df0c50af8434b2866f8b66f140e5c1493e2d
terehsieh <42392544+terehsieh@users.noreply.github.com>
2020-11-29 23:46:34 -0300

    implements observer and observable

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, observable } from 'rxjs';
 import { ProductCartService } from '../product-cart.service';
+import { Product } from '../product-list/Product';
 
 @Component({
   selector: 'app-cart',
@@ -7,8 +9,12 @@ import { ProductCartService } from '../product-cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor(private cart: ProductCartService) { }
+cartList$:Observable<Product[]>; // convencio se pone signo pesos para saber que es un observable y no un dato real
+  constructor(private cart: ProductCartService) { 
+    //suscribir
+    this.cartList$=cart.cartList.asObservable(); //definir q es un observable
+    // cart.cartList.subscribe((observable)=>this.cartList= observable);
+  }
 
   ngOnInit(): void {
   }
--- src/app/product-cart.service.ts
+++ src/app/product-cart.service.ts
@@ -1,25 +1,29 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Product } from './product-list/Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductCartService {
-
-  cartList: Product[] = [];//arreglo de productos
+  private _cartList: Product[]=[];//variable observada, por convencion se escribe guion abajo
+//observable
+  cartList: BehaviorSubject<Product[]> = new BehaviorSubject(this._cartList);//pasa por parametro el array de product
 
   constructor() { }
 
   addToCart(product: Product) {
 //arrow functions: pasa por parametro el valor, y luego hace la comparacion
-  let item: Product =  this.cartList.find((v1)=>v1.name ==product.name);
+  let item: Product =  this._cartList.find((v1)=>v1.name ==product.name);
   //si no encontro el item, es decir es item ===null
   if(!item){// si pongo dos veces en comprar, no la agrega dos veces
-    this.cartList.push({... product});// si no esta, la agrego. los puntitos esta clonando un objeto, hace una push de la copia
+    this._cartList.push({... product});// si no esta, la agrego. los puntitos esta clonando un objeto, hace una push de la copia
   }else{
   item.quantity+=product.quantity; // sumo la cantidad que tenia anteriormente mas la cantidad a insertar
   }
-  console.log(this.cartList);
+  console.log(this._cartList);
+
+  this.cartList.next(this._cartList);// emite el nuevo valor, esto es equivalente al emit de eventos
   }
 
 }
